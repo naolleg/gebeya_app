5b64a5605193e89399401734189d0d75d65cfb85
naolleg <naollegesse51@gmail.com>
2024-07-18 16:14:44 +0300

    made a validation to the login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -9,34 +9,47 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   String name = "";
   bool changeButton = false;
+  final _formkey = GlobalKey<FormState>();
+
+  void tohomepage(BuildContext context) async {
+    if (_formkey.currentState?.validate() ?? false) {
+      setState(() {
+        changeButton = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      Navigator.pushNamed(context, myRoute.homeroute);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
-        color: Colors.white,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.asset(
-                "assets/images/login_image.png",
-                fit: BoxFit.cover,
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome $name",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
               ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                "Welcome $name",
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 16.0, horizontal: 32.0),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Form(
+                key: _formkey,
                 child: Column(
                   children: [
                     TextFormField(
@@ -44,8 +57,14 @@ class _LoginState extends State<Login> {
                         hintText: "Enter username",
                         labelText: "Username",
                       ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "can not submit null username";
+                        }
+                        return null;
+                      },
                       onChanged: (value) {
-                        name = value;
+                        name = value ?? '';
                         setState(() {});
                       },
                     ),
@@ -55,24 +74,31 @@ class _LoginState extends State<Login> {
                         hintText: "Enter password",
                         labelText: "Password",
                       ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "can not submit null password";
+                        }
+                        if (value.length < 8) {
+                          return "password should be at least 8 characters";
+                        }
+                        return null;
+                      },
                     ),
                     SizedBox(
                       height: 40.0,
                     ),
-
                     InkWell(
-                      onTap: () async {
-                        setState(() {
-                          changeButton = true;
-                        });
-                        await Future.delayed(Duration(seconds: 1));
-                        Navigator.pushNamed(context, myRoute.homeroute);
-                      },
+                      onTap: () => tohomepage(context),
                       child: AnimatedContainer(
                         duration: Duration(seconds: 1),
                         width: changeButton ? 50 : 150,
                         height: 50,
                         alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius:
+                              BorderRadius.circular(changeButton ? 50 : 8),
+                        ),
                         child: changeButton
                             ? Icon(
                                 Icons.done,
@@ -85,26 +111,15 @@ class _LoginState extends State<Login> {
                                     fontWeight: FontWeight.bold,
                                     fontSize: 18),
                               ),
-                        decoration: BoxDecoration(
-                          color: Colors.deepPurple,
-                          borderRadius:
-                              BorderRadius.circular(changeButton ? 50 : 8),
-                        ),
                       ),
                     ),
-
-                    // ElevatedButton(
-                    //   child: Text("Login"),
-                    //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                    //   onPressed: () {
-                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
-                    //   },
-                    // )
                   ],
                 ),
-              )
-            ],
-          ),
-        ));
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
